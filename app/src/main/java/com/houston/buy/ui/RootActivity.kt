0b90b853e4574468645355676140603e77ef1a00
Саид <HUEY.news@yandex.ru>
2024-06-10 20:33:48 +0300

    issue#4: Добавил контейнер для фрагментов в activity_root.xml

--- app/src/main/java/com/houston/buy/ui/RootActivity.kt
+++ app/src/main/java/com/houston/buy/ui/RootActivity.kt
@@ -1,16 +1,17 @@
-package com.houston.buy
+package com.houston.buy.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.houston.buy.R
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

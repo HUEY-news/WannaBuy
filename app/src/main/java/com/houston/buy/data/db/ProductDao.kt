86becb7af25341acc3ed3569bd0fedfdf6c06b19
Саид <HUEY.news@yandex.ru>
2024-06-22 18:52:35 +0300

    issue#8: Расписал DAO интерфейс: описал методы для добавления продуктов в БД и удаления из неё.

---
+++ app/src/main/java/com/houston/buy/data/db/ProductDao.kt
@@ -0,0 +1,15 @@
+package com.houston.buy.data.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface ProductDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addProduct(entity: ProductEntity)
+
+    @Delete
+    suspend fun removeProduct(entity: ProductEntity)
+}

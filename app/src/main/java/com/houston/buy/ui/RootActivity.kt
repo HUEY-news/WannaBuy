6f09e934b166cb56dc43d599c8eabe1609e0aec7
Саид <HUEY.news@yandex.ru>
2024-07-23 13:00:13 +0300

    issue#18: Сменил пунктирную обводку на сплошную. Запретил ночную тему.

--- app/src/main/java/com/houston/buy/ui/RootActivity.kt
+++ app/src/main/java/com/houston/buy/ui/RootActivity.kt
@@ -2,6 +2,7 @@ package com.houston.buy.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 import com.houston.buy.R
 import com.houston.buy.databinding.ActivityRootBinding
@@ -14,6 +15,8 @@ class RootActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController

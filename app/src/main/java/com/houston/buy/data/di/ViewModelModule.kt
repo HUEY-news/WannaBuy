17d560904cd197c8948cd83c7274b9c828ae045c
Саид <HUEY.news@yandex.ru>
2024-07-24 16:02:37 +0300

    issue#22: Добавил новую viewmodel в DI.

--- app/src/main/java/com/houston/buy/data/di/ViewModelModule.kt
+++ app/src/main/java/com/houston/buy/data/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.houston.buy.data.di
 
 import com.houston.buy.presentation.ProductCreationViewModel
+import com.houston.buy.presentation.ProductDatabaseViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { ProductCreationViewModel(interactor = get()) }
+    viewModel { ProductDatabaseViewModel(interactor = get()) }
 }

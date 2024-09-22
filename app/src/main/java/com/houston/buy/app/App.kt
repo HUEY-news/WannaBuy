ce59e8fb1b2ebcc1cbc3937723fdd0b0927650e1
Саид <HUEY.news@yandex.ru>
2024-09-22 19:23:35 +0300

    issue#29: Добавил аннотацию @HiltAndroidApp в application класс.

--- app/src/main/java/com/houston/buy/app/App.kt
+++ app/src/main/java/com/houston/buy/app/App.kt
@@ -5,9 +5,11 @@ import com.houston.buy.di.dataModule
 import com.houston.buy.di.interactorModule
 import com.houston.buy.di.repositoryModule
 import com.houston.buy.di.viewModelModule
+import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
